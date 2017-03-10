b5285a65fcf64fadb624913a4e3c8f4cd9cd11a4
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2017-03-10 22:10:04 +0100

    Separate maven snapshots and releases urls. When both under the same maven scope just the latter will be considered.

--- rsc/test_smartcap.groovy
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-//bin/true && SCRIPT_DIR="$(dirname $(readlink -f "${0}"))"
-//bin/true && exec groovy -cp "${SCRIPT_DIR}/lib" "${0}" "${@}"; exit $?
-import it.italiaonline.rnd.filters.TextFilter;
-
-for ( String test : args ) {
-  new TextFilter.SmartCap(test).result()
-}
-
