b1f5fd4e9cd11fd5aaa2a5c27a33dd03f9671d9c
Giuseppe Ricupero <giuseppe.ricupero@gmail.com>
2022-05-31 16:15:21 +0200

    Updating the project to python 3

--- python/run.py
+++ python/run.py
@@ -4,28 +4,26 @@
 from TextFilter import SmartCap as sc
 
 import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
 args = sys.argv[1:]
 
 def usage():
-    print 'usage: ' + sys.argv[0] + ' default <one [or more words]>'
-    print '       ' + sys.argv[0] + ' address <one [or more words]>'
-    print '       ' + sys.argv[0] + ' tolerance <threshold> <one [or more words]>'
+    print(('usage: ' + sys.argv[0] + ' default <one [or more words]>'))
+    print(('       ' + sys.argv[0] + ' address <one [or more words]>'))
+    print(('       ' + sys.argv[0] + ' tolerance <threshold> <one [or more words]>'))
     sys.exit(1)
 
 if not len(args) > 0:
     usage()
 
 if args[0] == 'default':
-    input_string = u" ".join(args[1:])
-    print sc.smart_cap(input_string)
+    input_string = " ".join(args[1:])
+    print((sc.smart_cap(input_string)))
 elif args[0] == 'address':
-    input_string = u" ".join(args[1:])
-    print sc.address_smart_cap(input_string)
+    input_string = " ".join(args[1:])
+    print((sc.address_smart_cap(input_string)))
 elif args[0] == 'tolerance':
     threshold = args[1]
-    input_string = u" ".join(args[2:])
-    print sc.conditional_smart_cap(input_string,threshold)
+    input_string = " ".join(args[2:])
+    print((sc.conditional_smart_cap(input_string,threshold)))
 else:
     usage()
